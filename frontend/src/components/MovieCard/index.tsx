d3efd2bb0354d3bd85b092773728fe2e4da4c13d
SMarcosM <sebastiaomarcos49@gmail.com>
2022-05-12 17:24:32 -0300

    Correction

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import MovieScore from '../../components/MovieScore';
-import Form from '../../pages/Form';
+
 
 
 function MovieCard() {
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Listing from '../Listing';
+
 import './styles.css'
 
 function Form() {
